9561f66c6b59f9b8504996f7efa946caff1fe18d
DestinedCodes <yiradesat@gmail.com>
2023-07-17 15:50:27 +0100

    Fixed Betty

--- shell.c
+++ shell.c
@@ -8,48 +8,48 @@
 
 int main(int argc, char *argv[])
 {
-    char *line = NULL, *command[1024];
-    size_t len = 0, i = 0;
-    pid_t cpid;
-    struct stat st;
-    int read;
+	char *line = NULL, *command[1024];
+	size_t len = 0, i = 0;
+	pid_t cpid;
+	struct stat st;
+	int read;
 
-    while (1)
-    {
-        if (isatty(0))
-            printf("#cisfun ");
+	while (1)
+	{
+		if (isatty(0))
+			printf("#cisfun ");
 
-        read = getline(&line, &len, stdin);
+		read = getline(&line, &len, stdin);
 
-        if (read == -1)
-            break;
+		if (read == -1)
+			break;
 
-        command[0] = strtok(line, " \n");
+		command[0] = strtok(line, " \n");
 
-        for (i = 1; command[i - 1]; i++)
-            command[i] = strtok(NULL, " \n");
+		for (i = 1; command[i - 1]; i++)
+			command[i] = strtok(NULL, " \n");
 
-        if (command[0])
-        {
-            if (stat(command[0], &st) == 0)
-            {
-                cpid = fork();
-                if (cpid == -1)
-                {
-                    perror("Error: ");
-                    return (-1);
-                }
-                if (cpid == 0)
-                    execve(command[0], command, NULL);
-            }
-            else
-                printf("%s: No such file or directory\n", argv[0]);
-        }
+		if (command[0])
+		{
+			if (stat(command[0], &st) == 0)
+			{
+				cpid = fork();
+				if (cpid == -1)
+				{
+					perror("Error: ");
+					return (-1);
+				}
+				if (cpid == 0)
+					execve(command[0], command, NULL);
+			}
+			else
+				printf("%s: No such file or directory\n", argv[0]);
+		}
 
-        wait(&cpid);
-    }
+		wait(&cpid);
+	}
 
-    free(line);
+	free(line);
 
-    return (0);
+	return (0);
 }
