70286b147974f0a2f08798cc977a4f9e9bd5dd90
DestinedCodes <yiradesat@gmail.com>
2023-07-19 02:07:07 +0100

    Fix absulute path

--- findpath.c
+++ findpath.c
@@ -1,13 +1,16 @@
 #include "shell.h"
 
 char *findpath(path *head, char *command) {
+    if (access(command, X_OK) == 0) {
+        return strdup(command);
+    }
+
     path *current = head;
 
     while (current != NULL) {
         char abs_path[1024];
         snprintf(abs_path, sizeof(abs_path), "%s/%s", current->dir, command);
-        struct stat st;
-        if (stat(abs_path, &st) == 0) {
+        if (access(abs_path, X_OK) == 0) {
             return strdup(abs_path);
         }
         current = current->next;
