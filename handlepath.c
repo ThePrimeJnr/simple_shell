242e29d25ffef96ff1f79e809f50c79194d5930b
Heavenboi <nigelzenda98@gmail.com>
2023-07-19 11:17:10 -0700

    fixed bety

--- handlepath.c
+++ handlepath.c
@@ -1,67 +1,77 @@
 #include "shell.h"
-
+/**
+ *
+ * 
+ */
 path *initpath(path *head, char *env[])
 {
-    char *var, *value;
-    path *temp;
+	char *var, *value;
+	path *temp;
 
-    head = NULL;
+	head = NULL;
 
-    var = strtok(env[0], "=");
+	var = strtok(env[0], "=");
 
-    for (int i = 1; env[i]; i++)
-    {
-        var = strtok(env[i], "=");
+	for (int i = 1; env[i]; i++)
+	{
+		var = strtok(env[i], "=");
 
-        if (!strcmp(var, "PATH"))
-        {
-            value = strtok(NULL, "=");
+		if (!strcmp(var, "PATH"))
+		{
+			value = strtok(NULL, "=");
 
-	    temp = malloc(sizeof(path));
-            temp->dir = strtok(value, ":");
-            while(temp->dir)
-            {
-                temp->next = head;
-                head = temp;
+			temp = malloc(sizeof(path));
+			temp->dir = strtok(value, ":");
+			while (temp->dir)
+			{
+				temp->next = head;
+				head = temp;
 
-		temp = malloc(sizeof(path));
-                temp->dir = strtok(NULL, ":");
-            }
-            break;
-        }
-    }
+				temp = malloc(sizeof(path));
+				temp->dir = strtok(NULL, ":");
+			}
+			break;
+		}
+	}
 
-    return head;
+	return (head);
 }
 
-char *findpath(path *head, char *command) {
-    if (access(command, X_OK) == 0) {
-        return strdup(command);
-    }
+char *findpath(path *head, char *command)
+{
+	if (access(command, X_OK) == 0)
 
-    path *current = head;
+		return (strdup(command));
 
-    while (current != NULL) {
-        char abs_path[1024];
-        strcpy(abs_path, current->dir);
-        strcat(abs_path, "/");
-        strcat(abs_path, command);
-        if (access(abs_path, X_OK) == 0) {
-            return strdup(abs_path);
-        }
-        current = current->next;
-    }
 
-    return NULL;
+	path *current = head;
+
+	while (current != NULL)
+	{
+		char abs_path[1024];
+
+		strcpy(abs_path, current->dir);
+		strcat(abs_path, "/");
+		strcat(abs_path, command);
+		if (access(abs_path, X_OK) == 0)
+		{
+			return (strdup(abs_path));
+		}
+		current = current->next;
+	}
+
+	return (NULL);
 }
 
 path *printlist(path *head)
 {
-    path *current = head;
-    while (current != NULL) {
-        printf("%s\n", current->dir);
-        current = current->next;
-    }
+	path *current = head;
 
-    return head;
+	while (current != NULL)
+	{
+		printf("%s\n", current->dir);
+		current = current->next;
+	}
+
+	return (head);
 }
