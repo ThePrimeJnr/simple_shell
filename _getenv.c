21162c79a0c757766e48d88907e3584ba5028f8e
Destiny Saturday <84413505+DestinedCodes@users.noreply.github.com>
2023-07-26 08:31:33 +0100

    Update _getenv.c

--- _getenv.c
+++ _getenv.c
@@ -8,12 +8,12 @@
  */
 char *_getenv(char *var)
 {
-	int i;
+	int i, str_len = _strlen(var);
 	char *value = NULL;
 
 	for (i = 0; environ[i]; i++)
 	{
-		if (!_strncmp(environ[i], var, _strlen(var)) && environ[i][_strlen(var)] == '=')
+		if (!_strncmp(environ[i], var, str_len) && environ[i][str_len] == '=')
 		{
 			value = environ[i] + _strlen(var) + 1;
 			return (value);
