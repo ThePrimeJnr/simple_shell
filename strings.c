99e7d0883db0f875e158cbf1b9772eddaba8a755
Destiny Saturday <84413505+DestinedCodes@users.noreply.github.com>
2023-07-26 08:05:30 +0100

    Update strings.c

--- strings.c
+++ strings.c
@@ -89,3 +89,21 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (0);
 }
+
+/**
+ * _strlen - returns the lenth of a string
+ * @s: Paremeter to count
+ * Return: length.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
