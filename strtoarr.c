eb8650fbed583d20239cb44d95d17c1b681e89d6
Destiny Saturday <84413505+DestinedCodes@users.noreply.github.com>
2023-07-26 08:33:21 +0100

    Update strtoarr.c

--- strtoarr.c
+++ strtoarr.c
@@ -55,10 +55,10 @@ int _strncmp(const char *str1, const char *str2, size_t n)
 	for (i = 0; i < n; i++)
 	{
 		if (str1[i] == '\0' || str2[i] == '\0')
-			return str1[i] - str2[i];
+			return (str1[i] - str2[i]);
 
 		if (str1[i] != str2[i])
-			return str1[i] - str2[i];
+			return (str1[i] - str2[i]);
 	}
-	return 0;
+	return (0);
 }
